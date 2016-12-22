48385677e7f51b6e7f7b561e4a2ee9075a49fc79
DannyLocke <dlocke@mac.com>
2016-12-22 13:43:44 -0600

    added junit and h2database

--- src/com/ironyard/Main.java
+++ src/com/ironyard/Main.java
@@ -1,20 +1,26 @@
 package com.ironyard;
 
+import org.h2.tools.Server;
 import spark.ModelAndView;
 import spark.Session;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Main {
 
-    static HashMap<String, User> users = new HashMap<>();
-    static ArrayList<Message> messages = new ArrayList<>();
+//    static HashMap<String, User> users = new HashMap<>();
+//    static ArrayList<Message> messages = new ArrayList<>();
 
-    public static void main(String[] args) {
-        addTestUsers();
-        addTestMessages();
+    public static void main(String[] args) throws SQLException{
+        Server.createWebServer().start();
+        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
+        createTables(conn);
+
+        addTestUsers(conn);
+        addTestMessages(conn);
 
         Spark.init();
 
@@ -29,18 +35,14 @@ public class Main {
                     }
 
                     HashMap m = new HashMap();
-                    ArrayList<Message> threads = new ArrayList<>();
-                    for (Message message : messages){
-                        if(message.replyId == replyIdNum) {
-                            threads.add(message);
-                        }
-                    }
+                    ArrayList<Message> threads = selectReplies(conn, replyIdNum);
 
                     Session session = request.session();
                     String userName = session.attribute("userName");
 
                     m.put("messages", threads);
                     m.put("userName", userName);
+                    m.put("replyId", replyIdNum);
                     return  new ModelAndView(m, "home.html");
                 }),
                 new MustacheTemplateEngine()
@@ -55,10 +57,9 @@ public class Main {
                         throw new Exception("Login name not found.");
                     }
 
-                    User user = users.get(userName);
+                    User user = selectUser(conn, userName);
                     if (user == null){
-                        user = new User (userName);
-                        users.put(userName, user);
+                        insertUser(conn, userName, "");
                     }
                     Session session = request.session();
                     session.attribute("userName", userName);
@@ -67,6 +68,34 @@ public class Main {
                     return "";
                 })
         );//end Spark /login
+
+
+        Spark.post(
+                "/create-message",
+                ((request, response) -> {
+                    Session session = request.session();
+                    String userName = session.attribute("userName");
+                    if (userName == null) {
+                        throw new Exception("Not logged in.");
+                    }
+
+                    String text = request.queryParams("messageText");
+                    String replyId = request.queryParams("replyId");
+                    if (text == null || replyId == null) {
+                        throw new Exception("Didn't get necessary query parameters.");
+                    }
+
+                    int replyIdNum = Integer.valueOf(replyId);
+
+                    //get user from database
+                    User user = selectUser(conn, userName);
+                    //insert message into database
+                    insertMessage(conn, user.id, replyIdNum, text);
+
+                    response.redirect(request.headers("Referer"));
+                    return "";
+                })
+        );
 
         //route for logout
         Spark.post(
@@ -81,17 +110,84 @@ public class Main {
 
     }//end main()
 
-    static void addTestUsers() {
-        users.put("Alice", new User("Alice"));
-        users.put("Bob", new User("Bob"));
-        users.put("Charlie", new User("Charlie"));
+    public static void addTestUsers(Connection conn) throws SQLException {
+        insertUser(conn, "Alice", "");
+        insertUser(conn, "Bob", "");
+        insertUser(conn, "Charlie", "");
     }
 
-    static void addTestMessages() {
-        messages.add(new Message(0, -1, "Alice", "Hello world!"));
-        messages.add(new Message(1, -1, "Bob", "This is another thread!"));
-        messages.add(new Message(2, 0, "Charlie", "Cool thread, Alice."));
-        messages.add(new Message(3, 2, "Alice", "Thanks"));
+    static void addTestMessages(Connection conn) throws SQLException {
+        insertMessage(conn, 1, -1, "Hello world!");
+        insertMessage(conn, 2, -1, "This is another thread.");
+        insertMessage(conn, 3, 1, "Cool thread, Alice!");
+        insertMessage(conn, 1, 2, "Thanks");
+    }
+
+
+
+    public static void createTables(Connection conn) throws SQLException {
+        Statement stmt = conn.createStatement();
+        stmt.execute("CREATE TABLE IF NOT EXISTS users (id IDENTITY, name VARCHAR, password VARCHAR)");
+        stmt.execute("CREATE TABLE IF NOT EXISTS messages (id IDENTITY, userID INT, reply_Id INT, text VARCHAR)");
+    }
+
+    public static void insertUser(Connection conn, String name, String password) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("INSERT INTO users VALUES (NULL, ?, ?)");
+        stmt.setString(1, name);
+        stmt.setString(2, password);
+        stmt.execute();
+    }
+
+    public static User selectUser(Connection conn, String name) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM users WHERE name = ?");
+        stmt.setString(1, name);
+        ResultSet results = stmt.executeQuery();
+        if (results.next()) {
+            int id = results.getInt("id");
+            String password = results.getString("password");
+            return new User(id, name, password);
+        }
+        return null;
+    }
+
+    public static void insertMessage(Connection conn, int userId, int replyId, String text) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("INSERT INTO messages VALUES (NULL, ?, ?, ?)");
+        stmt.setInt(1, userId);
+        stmt.setInt(2, replyId);
+        stmt.setString(3, text);
+        stmt.execute();
+    }
+
+    public static Message selectMessage(Connection conn, int id) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM messages " +
+                "INNER JOIN users ON messages.userID = users.id WHERE messages.id = ?");
+        stmt.setInt(1, id);
+        ResultSet results = stmt.executeQuery();
+        if (results.next()) {
+            int replyId = results.getInt("messages.reply_Id");  //qualified as we're using 2 tables
+            String name = results.getString("users.name");
+            String text = results.getString("messages.text");
+            return new Message(id, replyId, name, text);
+        }
+        return null;
+    }
+
+    public static ArrayList<Message> selectReplies (Connection conn, int replyId) throws SQLException {
+        ArrayList<Message> messages = new ArrayList<>();
+
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM messages " +
+                "INNER JOIN users ON messages.userID = users.id WHERE messages.reply_Id = ?");
+        stmt.setInt(1, replyId);
+        ResultSet results = stmt.executeQuery();
+
+        while(results.next()){
+            int id = results.getInt("messages.id");  //qualified as we're using 2 tables
+            String name = results.getString("users.name");
+            String text = results.getString("messages.text");
+            Message message = new Message (id, replyId, name, text);
+            messages.add(message);
+        }
+        return messages;
     }
 
 
--- src/com/ironyard/User.java
+++ src/com/ironyard/User.java
@@ -4,7 +4,17 @@ package com.ironyard;
  * Created by dlocke on 12/9/16.
  */
 public class User {
+
+    int id;
     String name;
+    String password;
+
+
+    public User(int id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
 
     public User(String name) {
         this.name = name;
